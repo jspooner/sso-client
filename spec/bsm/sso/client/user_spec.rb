14e52c763fa6137a2dab77c320dac9f74358f0c4
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2011-10-21 12:28:39 +0100

    Added spec

--- spec/bsm/sso/client/user_spec.rb
+++ spec/bsm/sso/client/user_spec.rb
@@ -33,6 +33,12 @@ describe Bsm::Sso::Client::User do
     a_request(:get, "https://sso.test.host/authorize?auth_token=TOK").should have_been_made
   end
 
+  it 'should authenticate with credentials' do
+    stub_request(:any, //).to_return(:body => "{}")
+    described_class.authenticate(:a => 1, :b => 2, :email => "email@example.com", :password => "secret")
+    a_request(:get, "https://sso.test.host/authenticate?email=email@example.com&password=secret").should have_been_made
+  end
+
   it 'should construct sign-in URLs' do
     described_class.sign_in_url(:a => '1').should == "https://sso.test.host/sign_in?a=1"
   end
