885e7d1613f39ccd9b424b99314d4c207a18fb82
Jonathan Spooner <jspooner@gmail.com>
2014-06-29 08:34:30 -0700

    Added support for Devise

--- lib/bsm/sso/client/railtie.rb
+++ lib/bsm/sso/client/railtie.rb
@@ -3,15 +3,16 @@ require 'rails'
 
 class Bsm::Sso::Client::Railtie < ::Rails::Railtie
   RESCUE_RESPONSES = { "Bsm::Sso::Client::UnauthorizedAccess" => :forbidden }.freeze
-
-  config.app_middleware.use RailsWarden::Manager do |manager|
-    manager.default_strategies :sso_ticket, :sso_http_auth
-    manager.failure_app = Bsm::Sso::Client::FailureApp
-    Bsm::Sso::Client.warden_configuration.call(manager) if Bsm::Sso::Client.warden_configuration
+  initializer 'bsm-sso.warden' do |app|
+    unless defined?(::Devise)
+      config.app_middleware.use RailsWarden::Manager do |manager|
+        manager.default_strategies :sso_ticket, :sso_http_auth
+        manager.failure_app = Bsm::Sso::Client::FailureApp
+        Bsm::Sso::Client.warden_configuration.call(manager) if Bsm::Sso::Client.warden_configuration
+      end
+    end
+    if config.action_dispatch.key?(:rescue_responses)
+      config.action_dispatch.rescue_responses.merge!(RESCUE_RESPONSES)
+    end
   end
-
-  if config.action_dispatch.key?(:rescue_responses)
-    config.action_dispatch.rescue_responses.merge!(RESCUE_RESPONSES)
-  end
-
 end
